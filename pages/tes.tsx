d0b2679ab938122909cac794cc3f5408f694a4a8
Harits Syah <haritssr@gmail.com>
2023-08-14 14:00:58 +0700

    add Tes page

---
+++ pages/tes.tsx
@@ -0,0 +1,13 @@
+import LayoutGlobalNavigation from "@/components/LayoutGlobalNavigation";
+
+export default function Latihan() {
+  return (
+    <LayoutGlobalNavigation
+      browserTitle="Tes"
+      description=""
+      globalNavigationTitle="Tes"
+    >
+      <div className=""></div>
+    </LayoutGlobalNavigation>
+  );
+}
