0af1df6f8623105eec5c6fa784e83992e221ced3
Harits Syah <haritssr@gmail.com>
2023-08-14 13:59:48 +0700

    add Referensi page

---
+++ pages/referensi.tsx
@@ -0,0 +1,13 @@
+import LayoutGlobalNavigation from "@/components/LayoutGlobalNavigation";
+
+export default function Latihan() {
+  return (
+    <LayoutGlobalNavigation
+      browserTitle="Referensi"
+      description=""
+      globalNavigationTitle="Referensi"
+    >
+      <div className=""></div>
+    </LayoutGlobalNavigation>
+  );
+}
