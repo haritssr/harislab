c9e899a07185437d4c02603d24478e372b8a816c
Harits Syah <haritssr@gmail.com>
2024-05-11 16:32:06 +0700

    adding hover:underline at <MainFeatureLink/>

--- src/components/MainFeatureLink.tsx
+++ src/components/MainFeatureLink.tsx
@@ -7,7 +7,7 @@ export default function MainFeatureLink({ to }: { to: string }) {
 	const pathname = usePathname();
 	const color = pathname.startsWith(`/${to.toLowerCase()}`)
 		? "text-blue-600"
-		: "text-zinc-600 hover:text-zinc-400";
+		: "text-zinc-700 hover:underline";
 	const LinkStyle = `duration-900 ${color}`;
 
 	return (
